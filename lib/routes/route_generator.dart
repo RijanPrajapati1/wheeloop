a789e6d92dcdfd51799705a03bcdd8fde23f11e8
RijanPrajapati1 <122766161+RijanPrajapati1@users.noreply.github.com>
2024-12-11 14:18:30 +0545

    defining route in route_generator for signup_screen

--- lib/routes/route_generator.dart
+++ lib/routes/route_generator.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:wheeloop/screen/login/login_screen.dart';
+import 'package:wheeloop/screen/signup/signup_screen.dart';
 
 class RouteGenerator extends StatelessWidget {
   const RouteGenerator({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: (LoginScreen()),
+    return MaterialApp(
+      initialRoute: '/',
       debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context) => const LoginScreen(),
+        '/signup': (context) => const SignupScreen(),
+      },
     );
   }
 }
