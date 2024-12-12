a97b1c363b6c5e0cbbe53fc799e556b134061f44
RijanPrajapati1 <122766161+RijanPrajapati1@users.noreply.github.com>
2024-12-12 17:50:40 +0545

    routings managed

--- lib/routes/route_generator.dart
+++ lib/routes/route_generator.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:wheeloop/screen/login/login_screen.dart';
-import 'package:wheeloop/screen/signup/signup_screen.dart';
+import 'package:wheeloop/screen/splash/splash_screen.dart';
 
 class RouteGenerator extends StatelessWidget {
   const RouteGenerator({super.key});
@@ -10,10 +9,7 @@ class RouteGenerator extends StatelessWidget {
     return MaterialApp(
       initialRoute: '/',
       debugShowCheckedModeBanner: false,
-      routes: {
-        '/login': (context) => const LoginScreen(),
-        '/signup': (context) => const SignUpScreen(),
-      },
+      routes: {'/': (context) => const SplashScreen()},
     );
   }
 }
