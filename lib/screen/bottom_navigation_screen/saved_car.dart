235572e8620028bf6ba8340a26e7f3cb50d436ce
RijanPrajapati1 <122766161+RijanPrajapati1@users.noreply.github.com>
2024-12-20 14:47:00 +0545

    bottom navigator completed

---
+++ lib/screen/bottom_navigation_screen/saved_car.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SavedCar extends StatelessWidget {
+  const SavedCar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('Iam a notification'));
+  }
+}
